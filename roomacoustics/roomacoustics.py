0218a104798d0ec61f90c507be34c205c3de5534
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2019-05-15 10:50:38 +0200

    added air attenuation calculation, also added to edc calculation

--- roomacoustics/roomacoustics.py
+++ roomacoustics/roomacoustics.py
@@ -85,6 +85,8 @@ def schroeder_integration(impulse_response, is_energy=False):
     """
     if not is_energy:
         data = np.abs(impulse_response)**2
+    else:
+        data = np.abs(impulse_response)
 
     if data.ndim >= 2:
         energy_decay_curve = np.fliplr(np.cumsum(np.fliplr(data), axis=-1))
@@ -96,7 +98,8 @@ def schroeder_integration(impulse_response, is_energy=False):
 
 def energy_decay_curve_analytic(
         surfaces, alphas, volume, times, source=None,
-        receiver=None, method='eyring', c=343.4, frequency=None):
+        receiver=None, method='eyring', c=343.4, frequency=None,
+        air_absorption=True):
     """Calculate the energy decay curve analytically by using Eyring's or
     Sabine's equation _[2].
 
@@ -138,7 +141,11 @@ def energy_decay_curve_analytic(
     surface_room = np.sum(surfaces)
     alpha_mean = np.sum(surfaces*alphas) / surface_room
 
-    m = 0
+    if air_absorption:
+        m = air_attenuation_coefficient(frequency)
+    else:
+        m = 0
+
     if all([source, receiver]):
         dist_source_receiver = np.linalg.norm(
             source.cartesian - receiver.cartesian)
@@ -159,3 +166,58 @@ def energy_decay_curve_analytic(
 
 
     return energy_decay_curve
+
+
+def air_attenuation_coefficient(frequency, temperature=20, humidity=50, atmospheric_pressure=101325):
+    """Calculate the attenuation coefficient m for the absorption caused by friction with the surrounding air.
+
+    Parameters
+    ----------
+    frequency : double
+        The frequency for which the attenuation coefficient is calculated.
+        When processing in fractional octave bands use the center frequency.
+    temperature : double
+        Temperature in degrees Celsius.
+    humidity : double
+        Humidity in percent.
+    atmospheric_pressure : double
+        Atmospheric pressure.
+
+    Returns
+    -------
+    attenuation_coefficient : double
+        The resulting attenuation coefficient.
+
+    """
+    roomTemperatureKelvin = temperature + 273.16
+    referencePressureKPa = 101.325
+    pressureKPa = atmospheric_pressure/1000.0
+
+    #determine molar concentration of water vapor
+    tmp = (( 10.79586 * (1.0 - (273.16/roomTemperatureKelvin) )) -
+        (5.02808 * np.log10((roomTemperatureKelvin/273.16)) ) +
+        (1.50474 * 0.0001 * (1.0 - 10.0 ** (-8.29692*((roomTemperatureKelvin/273.16) - 1.0)))) +
+        (0.42873 * 0.001 * (-1.0 + 10.0 ** (-4.76955*(1.0 - (273.16/roomTemperatureKelvin))))) - 2.2195983)
+
+    molarConcentrationWaterVaporPercent = (humidity * 10.0 ** tmp) / (pressureKPa/referencePressureKPa)
+
+    # determine relaxation frequencies of oxygen and nitrogen
+    relaxationFrequencyOxygen = ((pressureKPa/referencePressureKPa) *
+        (24.0 + (4.04 * 10000.0 * molarConcentrationWaterVaporPercent *
+        ((0.02 + molarConcentrationWaterVaporPercent) / (0.391 + molarConcentrationWaterVaporPercent)))))
+
+    relaxationFrequencyNitrogen = ((pressureKPa/referencePressureKPa) *
+        ((roomTemperatureKelvin / 293.16) ** (-0.5)) *
+        (9.0 + 280.0 * molarConcentrationWaterVaporPercent *
+        np.exp(-4.17 * (( (roomTemperatureKelvin / 293.16) ** (-0.3333333)) - 1.0))))
+
+    airAbsorptionCoeff = (((frequency**2) *
+        ((1.84 * 10.0**(-11.0) * (referencePressureKPa / pressureKPa) * (roomTemperatureKelvin/293.16)**0.5) +
+        ((roomTemperatureKelvin/293.16)**(-2.5) * (
+        ((1.278 * 0.01 * np.exp( (-2239.1/roomTemperatureKelvin))) /
+        (relaxationFrequencyOxygen + ((frequency**2)/relaxationFrequencyOxygen))) +
+        ((1.068 * 0.1 * np.exp((-3352.0/roomTemperatureKelvin))/
+        (relaxationFrequencyNitrogen + ((frequency**2)/relaxationFrequencyNitrogen)))))))
+        )* (20.0 / np.log(10.0)) / ((np.log10(np.exp(1.0))) * 10.0)) # Neper/m -> dB/m
+
+    return airAbsorptionCoeff
