2c1da245b168ff1cc6574d45578d6ffea47d9afe
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2019-11-04 10:51:13 +0100

    added test for rt estimation without regression

--- tests/test_rt.py
+++ tests/test_rt.py
@@ -4,8 +4,6 @@
 """ Tests for reverberation time related things. """
 
 import numpy as np
-from numpy import array
-
 import numpy.testing as npt
 
 import roomacoustics as ra
@@ -20,4 +18,17 @@ def test_rt_from_edc():
     TX = ['T20', 'T30', 'T40', 'T50', 'T60']
     for T in TX:
         RT_est = ra.reverberation_time_energy_decay_curve(edc_exp, times, T=T)
+        npt.assert_allclose(RT_est, RT)
+
+
+def test_rt_from_edc_no_regression():
+    times = np.linspace(0, 1.5, 2**9)
+    m = -60
+    edc = times * m
+    edc_exp = 10**(edc/10)
+    RT = 1.
+    TX = ['T20', 'T30', 'T40', 'T50', 'T60']
+    for T in TX:
+        RT_est = ra.reverberation_time_energy_decay_curve(
+            edc_exp, times, T=T, regression=False)
         npt.assert_allclose(RT_est, RT)
