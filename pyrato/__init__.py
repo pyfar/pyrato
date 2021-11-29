e5606e1548f4f011aa88602c9d5b1dcce2988e6c
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2021-11-29 19:34:54 +0100

    removing deprecated builtin wrapper

--- pyrato/__init__.py
+++ pyrato/__init__.py
@@ -7,6 +7,42 @@ __email__ = 'marco.berzborn@akustik.rwth-aachen.de'
 __version__ = '0.2.1'
 
 
-from .pyrato import *
-from .dsp import *
-from .edc import *
+from .roomacoustics import (
+    reverberation_time_energy_decay_curve,
+    schroeder_integration,
+    energy_decay_curve_analytic,
+    air_attenuation_coefficient,
+)
+from .dsp import (
+    find_impulse_response_maximum,
+    find_impulse_response_start,
+    filter_fractional_octave_bands,
+    time_shift,
+    center_frequencies_octaves,
+    center_frequencies_third_octaves
+)
+from .edc import (
+    energy_decay_curve_chu,
+    energy_decay_curve_chu_lundeby,
+    energy_decay_curve_lundeby,
+    energy_decay_curve_truncation,
+    estimate_noise_energy,
+)
+
+__all__ = [
+    'reverberation_time_energy_decay_curve',
+    'schroeder_integration',
+    'energy_decay_curve_analytic',
+    'air_attenuation_coefficient',
+    'find_impulse_response_maximum',
+    'find_impulse_response_start',
+    'filter_fractional_octave_bands',
+    'time_shift',
+    'center_frequencies_octaves',
+    'center_frequencies_third_octaves',
+    'energy_decay_curve_chu',
+    'energy_decay_curve_chu_lundeby',
+    'energy_decay_curve_lundeby',
+    'energy_decay_curve_truncation',
+    'estimate_noise_energy'
+]
