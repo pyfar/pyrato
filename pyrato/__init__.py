1de5fe11e96d06cf1f097999b3fe176fa4544db0
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2021-12-06 10:25:32 +0100

    Bump version: 0.2.1 → 0.3.0

--- pyrato/__init__.py
+++ pyrato/__init__.py
@@ -5,7 +5,7 @@
 __author__ = \
     """Marco Berzborn - Institute for Hearing Technology and Acoustics"""
 __email__ = 'marco.berzborn@akustik.rwth-aachen.de'
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 
 from .roomacoustics import (
