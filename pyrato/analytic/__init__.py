0aca9161ece3bcd44ea13f3a8b419ecd14f05838
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2022-05-02 18:22:16 +0200

    move analytic model

---
+++ pyrato/analytic/__init__.py
@@ -0,0 +1,9 @@
+from .analytic import (
+    rectangular_room_rigid_walls,
+    eigenfrequencies_rectangular_room_rigid
+)
+
+__all__ = (
+    'rectangular_room_rigid_walls',
+    'eigenfrequencies_rectangular_room_rigid'
+)
