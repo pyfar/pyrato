2cc8e95012ce33b5cc731c23fcccca386334b41e
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2024-03-20 15:26:44 +0100

    add supported versions to setup.py

--- setup.py
+++ setup.py
@@ -43,6 +43,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     description="Collection of functions commonly used in room acoustics",
     install_requires=requirements,
