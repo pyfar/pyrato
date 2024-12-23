7da504c3f264071a0ecb4e499be79db9a688341f
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2024-12-23 18:14:53 +0100

    rewrite init based on current files

--- pyrato/__init__.py
+++ pyrato/__init__.py
@@ -5,44 +5,24 @@ __author__ = \
 __email__ = 'info@pyfar.org'
 __version__ = '0.3.2'
 
-from .rap import (
-    reverberation_time_linear_regression
-)
+
 from .roomacoustics import (
     reverberation_time_energy_decay_curve,
     energy_decay_curve_analytic,
     air_attenuation_coefficient,
 )
-from .dsp import (
-    find_impulse_response_maximum,
-    find_impulse_response_start,
-    time_shift,
-    preprocess_rir,
-    estimate_noise_energy,
-)
-from .edc import (
-    schroeder_integration,
-    energy_decay_curve_chu,
-    energy_decay_curve_chu_lundeby,
-    energy_decay_curve_lundeby,
-    energy_decay_curve_truncation,
-    intersection_time_lundeby,
-)
+
+from . import edc
+from . import rap
+from . import dsp
+from . import analytic
 
 __all__ = [
-    'reverberation_time_linear_regression',
+    'edc',
+    'rap',
+    'dsp',
+    'analytic',
     'reverberation_time_energy_decay_curve',
-    'schroeder_integration',
     'energy_decay_curve_analytic',
     'air_attenuation_coefficient',
-    'find_impulse_response_maximum',
-    'find_impulse_response_start',
-    'time_shift',
-    'preprocess_rir',
-    'energy_decay_curve_chu',
-    'energy_decay_curve_chu_lundeby',
-    'energy_decay_curve_lundeby',
-    'energy_decay_curve_truncation',
-    'estimate_noise_energy',
-    'intersection_time_lundeby',
 ]
