7a831c7e18c61d4aa38560487b2ffc08382f508f
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2025-01-13 17:31:29 +0100

    adapt import to new parametric submodule

--- pyrato/__init__.py
+++ pyrato/__init__.py
@@ -6,9 +6,7 @@ __email__ = 'info@pyfar.org'
 __version__ = '0.3.2'
 
 
-from .roomacoustics import (
-    reverberation_time_energy_decay_curve,
-    energy_decay_curve_analytic,
+from .parametric import (
     air_attenuation_coefficient,
 )
 
@@ -16,6 +14,7 @@ from . import edc
 from . import rap
 from . import dsp
 from . import analytic
+from . import parametric
 
 __all__ = [
     'edc',
@@ -25,4 +24,5 @@ __all__ = [
     'reverberation_time_energy_decay_curve',
     'energy_decay_curve_analytic',
     'air_attenuation_coefficient',
+    'parametric',
 ]
