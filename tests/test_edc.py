6708dac7de7ac1c8b37413b7d15dbd7b1b83d678
Anne Heimes <64446926+ahms5@users.noreply.github.com>
2025-09-12 12:06:49 +0200

    fix failing tests from merge

--- tests/test_edc.py
+++ tests/test_edc.py
@@ -18,7 +18,7 @@ def test_schroeder_integration(is_energy):
     """Simple impulse response."""
     factor = 2 if is_energy else np.sqrt(2)
     data = pf.Signal(np.ones((1, 10))*factor, sampling_rate=10)
-    energy_decay_curve = ra.schroeder_integration(data, is_energy=is_energy)
+    energy_decay_curve = ra.edc.schroeder_integration(data, is_energy=is_energy)
 
     # test if the shape is correct
     npt.assert_array_equal(energy_decay_curve.time.shape, data.time.shape)
@@ -33,7 +33,7 @@ def test_schroeder_integration_multi_cdim(is_energy):
     """Simple impulse response."""
     factor = 2 if is_energy else np.sqrt(2)
     data = pf.Signal(np.ones((2, 1, 10))*factor, sampling_rate=10)
-    energy_decay_curve = ra.schroeder_integration(data, is_energy=is_energy)
+    energy_decay_curve = ra.edc.schroeder_integration(data, is_energy=is_energy)
 
     # test if the shape is correct
     npt.assert_array_equal(energy_decay_curve.time.shape, data.time.shape)
@@ -42,7 +42,6 @@ def test_schroeder_integration_multi_cdim(is_energy):
     truth = (np.arange(10, dtype=float)[::-1] + 1) * 2
     npt.assert_almost_equal(energy_decay_curve.time[0, 0], truth)
     npt.assert_almost_equal(energy_decay_curve.time[1, 0], truth)
-
 
 
 def test_edc_eyring():
