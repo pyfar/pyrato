aca2f6c96a241ad4103bfb24afa7b7ff428afe96
Maciek <maciej.jasinski@pw.edu.pl>
2025-11-05 14:33:29 +0100

    Added Sabine Reverberation Time

--- pyrato/parametric.py
+++ pyrato/parametric.py
@@ -151,3 +151,36 @@ def air_attenuation_coefficient(
             ) * 20.0 / np.log(10.0) / (np.log10(np.exp(1.0)) * 10.0))
 
     return air_abs_coeff
+
+def calculate_sabine_reverberation_time(surfaces, alphas, volume):
+
+    """Calculate the reverberation time using Sabine's equation.
+
+    Calculation according to [#]_.
+
+    Parameters
+    ----------
+    surfaces : ndarray, double
+        Surface areas of all surfaces in the room
+    alphas : ndarray, double
+        Absorption coefficients corresponding to each surface
+    volume : double
+        Room volume
+    
+    Returns
+    -------
+    reverberation_time_sabine :  double
+        The value of calculated reverberation time
+
+    References
+    ----------
+    .. [#] H. Kuttruff, Room acoustics, 4th Ed. Taylor & Francis, 2009.
+
+    """
+    alphas = np.asarray(alphas)
+    surfaces = np.asarray(surfaces)
+    absorption_area = np.sum(surfaces*alphas)
+
+    reverberation_time_sabine = 0.161*volume/(absorption_area)
+
+    return reverberation_time_sabine
