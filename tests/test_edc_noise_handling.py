f3f5d8bf0c5fec166a28927a8a6c32393c02fe72
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2026-02-10 14:01:37 +0100

    rename truncate_energy_decay_curve (#124)
    ### Which issue(s) are closed by this pull request?
    
    Closes #122 
    
    ### Changes proposed in this pull request:
    
    - rename `truncate_energy_decay_curve` to `threshold_energy_decay_curve`
    - add/improve docstring
    
    Co-authored-by: Marco Berzborn <mberz@users.noreply.github.com>

--- tests/test_edc_noise_handling.py
+++ tests/test_edc_noise_handling.py
@@ -263,7 +263,7 @@ def test_intersection_time_2D():
     npt.assert_allclose(actual, expected)
 
 
-def test_energy_decay_curve_threshold():
+def test__threshold_energy_decay_curve():
 
     t_60 = 1
     m = -60/t_60
@@ -274,7 +274,7 @@ def test_energy_decay_curve_threshold():
 
     edc_log = np.tile(edc_log, (2, 3, 1))
 
-    edc = enh._truncate_energy_decay_curve(10**(edc_log.copy()/10), 30)
+    edc = enh._threshold_energy_decay_curve(10**(edc_log.copy()/10), 30)
 
     edc_ref = 10**(edc_log.copy()/10)
     edc_ref[..., n_samples//2:] = np.nan
@@ -282,7 +282,7 @@ def test_energy_decay_curve_threshold():
     npt.assert_allclose(edc, edc_ref)
 
 
-def test_truncate_energy_decay_curve():
+def test_threshold_energy_decay_curve():
     t_60 = 1
     m = -60/t_60
 
@@ -293,7 +293,7 @@ def test_truncate_energy_decay_curve():
     edc_log = np.tile(edc_log, (2, 3, 1))
 
     edc = pf.TimeData(10**(edc_log.copy()/10), times)
-    edc_trunc = enh.truncate_energy_decay_curve(edc, 30)
+    edc_trunc = enh.threshold_energy_decay_curve(edc, 30)
 
     edc_ref = 10**(edc_log.copy()/10)
     edc_ref[..., n_samples//2:] = np.nan
