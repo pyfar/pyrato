7c384eae00d9688dc25e502cbf0c8513ca3bf47b
Marco Berzborn <mberz@users.noreply.github.com>
2026-03-18 08:51:57 +0100

    Refactor: Reverberation time Sabine (#156)
    ### Changes proposed in this pull request:
    
    - Harmonize the reverberation time function based on Sabine's equation
    to match the already refactored function for Eyring's equation

--- tests/test_parametric.py
+++ tests/test_parametric.py
@@ -1,5 +1,6 @@
 """Tests for parametric related things."""
 
+import numpy as np
 import numpy.testing as npt
 import pytest
 import pyrato.parametric as parametric
@@ -41,53 +42,59 @@ def test_mean_free_path_wrong_surface_area():
         mean_free_path(100, -1)
 
 
-def test_calculate_sabine_reverberation_time():
-    alphas = [0.9, 0.1]
-    surfaces = [2, 5*2]
-    volume = 2*2*2
-    assert ra.parametric.calculate_sabine_reverberation_time(surfaces,
-            alphas, volume) == 0.46
+def test_reverberation_time_sabine():
+    volume = 2 * 2 * 2
+    surface_area = 2 + 5 * 2
+    mean_absorption = (2 * 0.9 + 5 * 2 * 0.1) / surface_area
+    npt.assert_allclose(
+        ra.parametric.reverberation_time_sabine(
+            volume, surface_area, mean_absorption),
+        0.46,
+        atol=1e-2,
+    )
 
-def test_sabine_array_size_match():
-    message = "Size of alphas and surfaces ndarray sizes must match."
-    alphas = [0.9, 0.1, 0.3]
-    surfaces = [2, 5*2]
-    volume = 2*2*2
+def test_sabine_speed_of_sound_non_positive():
+    message = "Speed of sound should be larger than 0"
+    volume = 8
+    surface_area = 12
+    mean_absorption = 0.2
     with pytest.raises(ValueError, match=message):
-        ra.parametric.calculate_sabine_reverberation_time(surfaces,
-                                                          alphas, volume)
+        ra.parametric.reverberation_time_sabine(
+            volume, surface_area, mean_absorption, speed_of_sound=0)
 
 def test_sabine_alphas_outside_range():
-    message=r"Absorption coefficient values must be in range"
-    alphas = [2, 0]
-    surfaces = [1, 1]
-    volume = 2*2*2
-    with pytest.raises(ValueError, match = message):
-        ra.parametric.calculate_sabine_reverberation_time(surfaces,
-                                                          alphas, volume)
+    message = r"mean_absorption should be between 0 and 1"
+    volume = 8
+    surface_area = 2
+    mean_absorption = [2, 0]
+    with pytest.raises(ValueError, match=message):
+        ra.parametric.reverberation_time_sabine(
+            volume, surface_area, mean_absorption)
 
 def test_sabine_negative_surfaces():
-    message = r"Surface areas cannot be negative"
-    alphas = [1, 0]
-    surfaces = [-1, 1]
-    volume = 2*2*2
+    message = r"Surface area should be larger than 0"
+    volume = 8
+    surface_area = -1
+    mean_absorption = 0.5
     with pytest.raises(ValueError, match=message):
-        ra.parametric.calculate_sabine_reverberation_time(surfaces,
-                                                        alphas, volume)
+        ra.parametric.reverberation_time_sabine(
+            volume, surface_area, mean_absorption)
 
 def test_sabine_negative_volume():
-    message=r"Volume cannot be negative."
-    alphas = [1, 0]
-    surfaces = [1, 1]
+    message = r"Volume should be larger than 0"
     volume = -3
+    surface_area = 2
+    mean_absorption = 0.5
     with pytest.raises(ValueError, match=message):
-        ra.parametric.calculate_sabine_reverberation_time(surfaces,
-                                                          alphas, volume)
+        ra.parametric.reverberation_time_sabine(
+            volume, surface_area, mean_absorption)
 
 def test_sabine_zero_absorption_area():
-    alphas = [1, 0]
-    surfaces = [0, 1]
+    mean_absorption = 0
+    surface_area = 1
     volume = 3
-    with pytest.raises(ZeroDivisionError):
-        ra.parametric.calculate_sabine_reverberation_time(surfaces,
-                                                        alphas, volume)
+    npt.assert_equal(
+        ra.parametric.reverberation_time_sabine(
+            volume, surface_area, mean_absorption),
+        np.inf,
+    )
